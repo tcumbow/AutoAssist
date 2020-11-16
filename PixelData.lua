325b83f61d773409b837df891c724cdc3ef36195
Tom Cumbow <tcumbow@icloud.com>
2020-11-15 20:36:39 -0500

    logic to use Accelerate all the time

--- PixelData.lua
+++ PixelData.lua
@@ -114,6 +114,8 @@ local function BigLogicRoutine()
 		SetPixel(MeditationSlotted)
 	elseif RapidManeuverSlotted and not MajorExpedition and StaminaPercent > 0.90 then
 		SetPixel(RapidManeuverSlotted)
+	elseif AccelerateSlotted and not MajorExpedition and MagickaPercent > 0.90 then
+		SetPixel(AccelerateSlotted)
 	elseif InCombat == true and not ImbueWeaponActive then
 		SetPixel(DoHeavyAttack)
 	elseif ReelInFish and not InCombat then
