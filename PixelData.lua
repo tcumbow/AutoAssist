3a8b4841653ae0292f5ce14e9a6dd4896411cf45
Tom Cumbow <tcumbow@icloud.com>
2020-11-24 07:00:29 -0500

    Remove escape bar logic

--- PixelData.lua
+++ PixelData.lua
@@ -165,7 +165,7 @@ local function BigLogicRoutine()
 		SetPixel(DoAbility(Meditation))
 	-- elseif SunFire.Slotted and MagickaPercent > 0.80 and InCombat and TargetIsEnemy then
 	-- 	SetPixel(DoAbility(SunFire))
-	elseif InCombat and EnemiesAround and not ImbueWeaponActive and not (Accelerate.Slotted and RapidManeuver.Slotted and BackBar) then
+	elseif InCombat and EnemiesAround and not ImbueWeaponActive then
 		SetPixel(DoHeavyAttack)
 	elseif ReelInFish and not InCombat then
 		SetPixel(DoReelInFish)
