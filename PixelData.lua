8a8102e0662e2fbb235ba0f783b996d759e22018
Tom Cumbow <tcumbow@icloud.com>
2020-11-26 08:27:59 -0500

    ForceShock

--- PixelData.lua
+++ PixelData.lua
@@ -68,6 +68,7 @@ local Accelerate = { }
 local WeaknessToElements = { }
 local SoulTrap = { }
 local DestructiveTouch = { }
+local ForceShock = { }
 
 local DoNothing = 0
 -- 1 thru 5 are used for doing abilities 1 thru 5, based on the number assigned in UpdateAbilitySlotInfo()
@@ -169,6 +170,8 @@ local function BigLogicRoutine()
 		SetPixel(DoAbility(Meditation))
 	-- elseif SunFire.Slotted and MagickaPercent > 0.80 and InCombat and TargetIsEnemy then
 	-- 	SetPixel(DoAbility(SunFire))
+	elseif ForceShock.Slotted and MagickaPercent > 0.80 and InCombat and TargetIsEnemy then
+		SetPixel(DoAbility(ForceShock))
 	elseif InCombat and EnemiesAround and not ImbueWeaponActive then
 		SetPixel(DoHeavyAttack)
 	elseif ReelInFish and not InCombat then
@@ -336,6 +339,7 @@ local function UpdateAbilitySlotInfo()
 	WeaknessToElements = { }
 	SoulTrap = { }
 	DestructiveTouch = { }
+	ForceShock = { }
 
 	for barNumIterator = 0, 1 do
 		for i = 3, 7 do
@@ -388,6 +392,9 @@ local function UpdateAbilitySlotInfo()
 			elseif AbilityName == "Destructive Touch" or AbilityName == "Shock Touch" or AbilityName == "Destructive Reach" or AbilityName == "Shock Reach" then
 				DestructiveTouch.Slotted = true
 				DestructiveTouch[barNumIterator] = i-2
+			elseif AbilityName == "Force Shock" or AbilityName == "Force Shock" or AbilityName == "Force Shock" or AbilityName == "Force Shock" then
+				ForceShock.Slotted = true
+				ForceShock[barNumIterator] = i-2
 			elseif AbilityName == "Inner Light" or AbilityName == "Radiant Aura" or AbilityName == "Puncturing Sweep" or AbilityName == "Blockade of Storms" or AbilityName == "" then -- do nothing, cuz we don't care about these abilities
 			else 
 				d("Unrecognized ability:"..AbilityName)
