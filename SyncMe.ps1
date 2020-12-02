7665a3919eb11adbd885fdc5fcfb909c2c7d5be8
Tom Cumbow <tcumbow@icloud.com>
2020-12-01 20:28:41 -0500

    Update SyncMe.ps1

--- SyncMe.ps1
+++ SyncMe.ps1
@@ -1,1 +1,1 @@
-unison .\ A:\PixelData\ -ignore 'Path {.git,.vscode,Thoughts.txt}' -ignore 'Name {*.prf,*.ps1}'
+unison .\ A:\PixelData\ -ignore 'Path {.git,.vscode,Thoughts.txt}' -ignore 'Name {*.prf,*.ps1}' -auto
