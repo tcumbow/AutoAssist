e57de36b64dce4b2702ac47d562ebcb5fede0b76
Tom Cumbow <tcumbow@icloud.com>
2020-12-07 19:41:35 -0500

    use SpellPower potion in any battle

--- PixelData/PixelData.lua
+++ PixelData/PixelData.lua
@@ -222,6 +222,8 @@ local function BigLogicRoutine()
 			SetPixel(DoAbility(CrystalFragments))
 
 	-- Combat: Medium Priority (Buffs, DoTs, Looting, Meditation)
+		elseif PotionName=="Essence of Spell Power" and not MajorProphecy and not SunFire.Slotted and not InnerLight.Slotted and InCombat then
+			SetPixel(DoQuickslot)
 		elseif EnergyOverload.Slotted and not EnergyOverloadActive and MagickaPercent < 0.40 and Ultimate > (EnergyOverload.Cost * 5) and InCombat then
 			SetPixel(DoAbility(EnergyOverload))
 		elseif Ritual.Slotted and not MinorMending and InCombat and MagickaPercent > 0.55 then
