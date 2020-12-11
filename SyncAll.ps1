ea5f49f8347d91d35da9fdbede5c69809f9a076f
Tom Cumbow <tcumbow@icloud.com>
2020-12-11 16:42:58 -0500

    SyncAll

---
+++ SyncAll.ps1
@@ -0,0 +1,5 @@
+unison .\AutoAssist A:\AutoAssist -auto -batch
+pause
+unison .\AutoAssist "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoAssist" -auto -batch
+pause
+unison .\AutoAssist R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoAssist -auto -batch
