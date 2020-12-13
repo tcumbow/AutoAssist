6f8cf0d3dd23c172674ca4ac9cbb24e0797774a0
Tom Cumbow <tcumbow@icloud.com>
2020-12-13 18:04:50 -0500

    Sprint config

--- AutoAssist/AutoAssist.lua
+++ AutoAssist/AutoAssist.lua
@@ -314,9 +314,9 @@ local function BigLogicRoutine()
 			SetPixel(DoAbility(RapidManeuver))
 		elseif Config.Expedition and Accelerate.Slotted and not MajorExpedition and MagickaPercent > 0.99 and Moving and not InCombat then
 			SetPixel(DoAbility(Accelerate))
-		elseif ShouldSprint and Moving and not Sprinting and not Crouching and StaminaPercent > 0.80 then
+		elseif Config.Sprint and ShouldSprint and Moving and not Sprinting and not Crouching and StaminaPercent > 0.80 then
 			SetPixel(DoSprint)
-		elseif Sprinting and (not ShouldSprint or StaminaPercent < 0.80) and Moving and not Crouching then
+		elseif Config.Sprint and Sprinting and (not ShouldSprint or StaminaPercent < 0.80) and Moving and not Crouching then
 			SetPixel(DoSprint)
 
 	-- End of Logic
