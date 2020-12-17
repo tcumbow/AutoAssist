8bc2f106de30794d7bc2868b25411e50014664ec
Tom Cumbow <tcumbow@icloud.com>
2020-12-16 19:55:20 -0500

    SyncAll

--- SyncAll.ps1
+++ SyncAll.ps1
@@ -1,5 +1,12 @@
-unison .\AutoAssist A:\AutoAssist -auto -batch
-pause
-unison .\AutoAssist "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoAssist" -auto -batch
-pause
-unison .\AutoAssist R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoAssist -auto -batch
+Write-Host
+Write-Host
+Write-Host
+unison .\AutoAssist A:\AutoAssist -auto
+Write-Host
+Write-Host
+Write-Host
+unison .\AutoAssist "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoAssist" -auto
+Write-Host
+Write-Host
+Write-Host
+unison .\AutoAssist R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoAssist -auto
